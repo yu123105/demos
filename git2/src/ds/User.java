4c449b8d8b740ded9557f12b6b6055797beaa58e
asiafrank <zhangxf05@163.com>
2013-11-05 20:38:20 +0800

    Encoding Test:添加了用户的Javabean.

---
+++ git2/src/ds/User.java
@@ -0,0 +1,38 @@
+package ds;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 13-11-5
+ * Time: 下午8:20
+ * To change this template use File | Settings | File Templates.
+ */
+public class User {
+    private int id;
+    private String username;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
